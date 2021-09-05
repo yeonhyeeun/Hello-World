a3fc19c96b723df0eddd8874452f16aadb36d843
YeonHyeEun <21900442@handong.edu>
2021-09-05 14:56:14 +0900

    commit 2 write source code

--- lab1/src/com/example/lab1/Main.java
+++ lab1/src/com/example/lab1/Main.java
@@ -1,10 +1,18 @@
 package com.example.lab1;
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Hello World!!!");
+	    System.out.println("이름 입력: ");
+	      
+	      Scanner sc = new Scanner(System.in);
+	      String name = sc.next();
+	      
+	      System.out.print("["+name+"]님 환영합니다~");
 		
 
 	}
